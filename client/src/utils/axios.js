f1cdf906fd72e2edd707139641e2da8d71930522
Yadu Krishnan <yadukrishnan247@gmail.com>
2024-12-20 18:43:57 +0530

    ready to deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8888',
+  baseURL: 'https://doctor-booking-app-backend-zgk4.onrender.com',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   },
